840ddc307a9d9e67cbbb15689eba49c0e3cf5f92
zhang <zhangjs2233@gmail.com>
2019-07-25 13:11:20 +0800

    123

--- makeCoinChanges.js
+++ makeCoinChanges.js
@@ -16,21 +16,19 @@ class MinCoinChanges {
     for (var i = 0; i < this.coins.length; i++) { //{5}
       var coin = this.coins[i];
       newAmount = amount - coin; //{6}
-      console.log('newAmount',newAmount)
-      if (newAmount >= 0) {
-        newMin =this.makeChange(newAmount); //{7}
-        console.log('newMin',newMin)
-      }
-      console.log('test')
-      if ( newAmount >= 0 &&(newMin.length < min.length - 1 || !min.length)&&(newMin.length || !newAmount))
-        {
-          min = [coin].concat(newMin); //{11}
-          console.log('new Min ' + min + ' for ' + amount);
-        }
-      }
-      return (this.cache[amount] = min); //{12}
-    };
-  }
-  var minCoinChange=new MinCoinChanges([1,5,9,23])
-   console.log(minCoinChange.makeChange(100));
 
+      if (newAmount >= 0) {
+        newMin = this.makeChange(newAmount); //{7}
+
+      }
+
+      if (newAmount >= 0 && (newMin.length < min.length - 1 || !min.length) && (newMin.length || !newAmount)) {
+        min = [coin].concat(newMin); //{11}
+        console.log('new Min ' + min + ' for ' + amount);
+      }
+    }
+    return (this.cache[amount] = min); //{12}
+  };
+}
+var minCoinChange = new MinCoinChanges([1, 5, 9, 23])
+console.log(minCoinChange.makeChange(100));
